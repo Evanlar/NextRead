c0bfe8a29ef414fdf872d527dcb7e37c49f9ba74
Evanlar <71794796+Evanlar@users.noreply.github.com>
2022-06-03 12:59:02 -0700

    Basic backend fully working with Test case
    GUI taking shape

--- index.js
+++ index.js
@@ -1,67 +1,76 @@
-/*  TODO
+/* TODO
 
-form, gets submitted by button
+create nice looking UI 
 
-on submit don't do default
+Make UI scalable from mobile and desktop 
 
-query and get results
+add other search modes, filter by author, genres, and best match (algo)
 
-on click, hide form and results */
+*/
+const input = document.querySelector('input')  
+const header = document.querySelector('#header')
+const allResults = document.querySelector('.results')
+const logo = document.querySelector('#logo')
+const displayHeight = window.innerHeight/7
+const displayWidth = window.innerWidth/2
+logo.style.setProperty('--element-height', displayHeight + 'px')
+logo.style.setProperty('--element-width', displayWidth + 'px')
+async function searchGenre(genre) {
+    const response = await axios.get(`https://gutendex.com/books?topic=${genre}`)
+    let results = response['data']['results'].slice(0, 20)
+    console.log(`searched ${genre}`, results)
+    organiseResults(results, 'genreResult')}
 
+
+//This fuction takes an array and generates clickable dropdown 
+function organiseResults(array, idType) {
+    console.log(array)
+    array.forEach(book => {
+        input.style.display='none';
+        let title = book['title'];
+        let author = book['authors'][0]['name'];
+        let genreList = book['bookshelves'];
+       
+        let tag = document.createElement('p');
+        tag.setAttribute('id', idType)
+        let text = document.createTextNode(author + ' - ' + title)
+        //if id === bookresult else ceate different ttag 
+        tag.addEventListener('click', () =>{
+             //if no genres to search fo 
+        if (genreList.length === 0) {
+            allResults.innerHTML="";
+            input.style.display='none';
+            header.innerHTML = `<h1>SORRY!!! No Books related to <b>${title}<b> by <b>${author}<b> \n please search again</h1>`;
+        }
+        else {
+            allResults.innerHTML="";
+            //This is book select
+            //grab needed search criteria
+            const relatedScreenTitle = `<h1>Books related to <b>${title}<b> by <b>${author}<b></h1>`;
+            console.log(relatedScreenTitle)
+            input.style.display='none';
+            
+
+            header.innerHTML = relatedScreenTitle;
+            searchGenre(genreList[0]);
+        }
+        })
+
+        tag.appendChild(text)
+        allResults.appendChild(tag)
+        //Make this pretty and output as clickable obj 
+        console.log(title,author,genreList);
+        })
+}
 
 async function searchBook(searchTerm) {
     const response = await axios.get(`https://gutendex.com/books?search=${searchTerm}`)
     let bookArray = response['data']['results']
     if (bookArray.length == 0) {console.error('No book results match this query');}
     console.log(`searched ${searchTerm}`, response)
-   
-    // loop needs to create new result spot to list books 
-    for (let i = 0; i < 20; i++) {
-    let book = bookArray[i];
-    if (book) {
-    console.log(bookArray[0])
-    let title = book['title'];
-    let author = book['authors'][0]['name'];
-
-    let tag = document.createElement('p');
-    let text = document.createTextNode(author + ' - ' + title)
-
-
-    tag.addEventListener('click', () =>{
-        //This is book select
-        console.log('I.ve been clicked')
-        //grab needed search criteria
-        const relatedScreenTitle = `Titles related to ${title}`;
-        //toggle between search and result screen
-        //title RESULTS RELATED TO TITLE
-        //genre etc 
-        //(same screen but diffrent content )
-        allResults.style.display='none';
-        input.style.display='none';
-        button.style.display='none';
-        //Format the title so it includes title and subtitle, then 
-        //here we can delete subtitle and create new title
-        header.innerHTML = `<h1>Results for ${title}</h1> `
-
-        //Change innerHTML of title screen 
-
-        //with saved info, search for related books (FUNC), then appendchild of each listing
-        
-      })
-    tag.appendChild(text)
-    allResults.appendChild(tag)
-    }}
-    //lets try to pop this out as a module, 2 column selector 
-    //this needs to be some HTML elm that holds title and author, when user clicks then will search by genre
-    
+    organiseResults(bookArray,'bookResult')
 }
-//not needed? looked like doc required it
-function switchSearchTerm(bookTitle) {
-    console.log(bookTitle)
-    if (!bookTitle.includes(' ')){return bookTitle}
-    formatedTitle = bookTitle.replace(' ', '%20')
-    return formatedTitle
-}
+
 //Now when we type, after a debounce, search is performed. 
 
 // Oninput is after debounce and returning it ready to be used 
@@ -69,17 +78,14 @@ function switchSearchTerm(bookTitle) {
 const onInput = async event => {
     let searchTerm =  await event.target.value;
     if (searchTerm != ' ' && searchTerm) {
-    console.log(searchTerm)
+    console.log('on input' , searchTerm)
     searchBook(searchTerm)
     }
 }
 
-const button = document.querySelector('#submit')
-const input = document.querySelector('input')  
-const header = document.querySelector('#header')
-const allResults = document.querySelector('.results')
 input.addEventListener('input', debounce(onInput, 1000));  
-header.innerHTML = "<h1>Enter a book you like</h1> <p>We'll find something similar to read</p>"
-button.addEventListener('click', function() {
-    //this needs to run constantly as typing
-    searchBook(switchSearchTerm(searchTerm.value))})
+
+//this will later on be a logo 
+
+
+//allResults.innerHTML = "<p>We'll find something similar to read</p>"
